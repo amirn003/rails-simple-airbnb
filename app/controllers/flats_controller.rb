ecbc8cac39b84d8195d385278237bc71c2490b1b
amirn003 <amir.naar@gmail.com>
2023-07-30 01:00:51 +0200

    add actions edit, update, destroy

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,10 +1,11 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
   end
 
   def show
-    @flat = Flat.find(params[:id])
   end
 
   def new
@@ -17,9 +18,29 @@ class FlatsController < ApplicationController
     redirect_to flat_path(@flat)
   end
 
+  def edit
+  end
+
+  def update
+    if @flat.update(params_set)
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flat_path, status: :see_other
+  end
+
   private
 
   def params_set
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
